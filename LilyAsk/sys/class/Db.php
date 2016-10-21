01c5515663562f849caebf5585c277749d6e75f8
Hiki <aneureka2@gmail.com>
2016-10-21 21:20:57 +0800

    建立大概的框架

---
+++ LilyAsk/sys/class/Db.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Hiki
+ * Date: 2016/10/21
+ * Time: 21:19
+ */
+class Db
+{
+
+}
---
+++ LilyAsk/sys/config/smarty_init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hiki
+ * Date: 2016/10/21
+ * Time: 20:56
+ */
+
+define('SMARTY_ROOT',LILY_ROOT.'front/');
+
+
+
